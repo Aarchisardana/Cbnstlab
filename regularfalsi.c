a7a03973737c13392e27691dcb515ca09d5292ca
Aarchi <152676568+Aarchisardana@users.noreply.github.com>
2024-10-03 19:29:09 +0530

    Update regularfalsi.c

--- regularfalsi.c
+++ regularfalsi.c
@@ -1,3 +1,9 @@
 // regular falsi method or false position method 
 //  the iteration will be given till where up to we have to go like till
 // 4 iteration
+// joh negative ara hae c kae formula mae rkhneh kae badh mujeh usko a sae replace krnah hae c kie value ko 
+//  hie krnah a sae replace 
+//  a=    b = 
+// f(a)    f(b)
+// c =  a*f(b) - b*f(b)  / f(b)-f(a)
+
