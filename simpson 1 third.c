35bbeb0d178c6542362c063f482959dca1d3ef86
Aarchi <152676568+Aarchisardana@users.noreply.github.com>
2024-12-10 16:48:45 +0530

    Simpson 1 third.c

--- simpson 1 third.c
+++ simpson 1 third.c
@@ -32,4 +32,50 @@ int main(){
    return 0;
     }
     
-      an sis same 
+    // ans is  same 
+
+
+
+
+
+
+
+
+
+
+
+
+  approach  when table  will bw given just take input  the vlaues  x and y 
+
+
+#include <stdio.h>
+int main() {
+    int n; 
+    float result = 0.0;
+    printf("Enter the number of subintervals: ");
+    scanf("%d", &n);
+    float  x[n + 1], f_values[n + 1];  // Arrays to store x values and corresponding f(x) values
+float  integration , h ;
+    printf("Enter the x values and corresponding function values (f(x)):\n");
+    for (int i = 0; i <= n; i++) {
+        printf("x[%d]: ", i);
+        scanf("%f", &x[i]);
+        printf("f(x[%d]): ", i);
+        scanf("%f", &f_values[i]);
+    }
+    h = (x[n] - x[0]) / n;
+   integration   = f_values[0] + f_values[n];  // Add the boundary values
+
+    for (int i = 1; i < n; i++) {
+        if (i % 2 == 0) {
+            integration =   integration + 2 * f_values[i];  // Even indices: multiply by 2
+        } else {
+             integration =  integration+ 4 * f_values[i];  // Odd indices: multiply by 4
+        }
+    }
+    result = (h / 3) *  integration;
+    printf("Required value of integration is: %f\n", result);
+    return 0;
+}
+
+
