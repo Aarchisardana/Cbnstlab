e509b65ddbb32445ea4f55839f1292b3cea47172
Aarchi <152676568+Aarchisardana@users.noreply.github.com>
2024-12-10 17:52:05 +0530

    Trapezoidal.c

--- trapezoidal.c
+++ trapezoidal.c
@@ -32,3 +32,36 @@
 //comments 
 and is 0.784241  kinldy check by putting this to verify
  for using pow(x,3) 
+
+
+
+
+
+
+
+
+ when table is given 
+
+#include <stdio.h>
+int main()
+{
+    int n     ;  // n= nsubinterval 
+ printf(" enter  the value of n ");
+ scanf("%d" , &n);
+ float x[n+1] , y[n+1]  , h ,  integration , k , result;
+ for(int i =0; i<=n ; i++){
+    printf("x[%d]:" ,i );
+    scanf("%f" ,&x[i]);
+    printf("f(x[%d]):" ,i );
+    scanf("%f" ,&y[i]);
+ }
+   h = (x[n] - x[0]) / n ;
+    integration =  y[0]  + y[n] ;
+ for(int i = 1; i <n; i++) {
+      k =  x[0] + h*i ;
+      integration =  integration + 2*y[i] ;
+ }
+  result  =  ( integration * h) /2 ;
+  printf("   so final  integration is %f" , result);
+ return 0;
+}
